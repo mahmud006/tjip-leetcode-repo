b6325d6874124c0bb00b139f4950ad7e3bdc6560
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-06-09 23:56:47 +0600

    added solutions

---
+++ week06/230-Kth_smallest_element_in_a_BST.cpp
@@ -0,0 +1,26 @@
+// TC - O(k)
+// MC - O(1)
+
+class Solution
+{
+public:
+    void runInorder(TreeNode *root, int &k, int &kthValue)
+    {
+        if (!root)
+            return;
+        runInorder(root->left, k, kthValue);
+        k--;
+        if (k == 0)
+        {
+            kthValue = root->val;
+            return;
+        }
+        runInorder(root->right, k, kthValue);
+    }
+    int kthSmallest(TreeNode *root, int k)
+    {
+        int kthValue;
+        runInorder(root, k, kthValue);
+        return kthValue;
+    }
+};
